07001cdaecb77e0f1451f87996db291bf9532a08
Zaino <hello@darrin.dev>
2021-04-07 12:39:41 -0700

    add successful connection to TMDB API

--- assets/js/media.js
+++ assets/js/media.js
@@ -1,16 +1,32 @@
 //*put our code for OMDB here
 
-//* API key = 759292a3
+//*OMDB
+//* Erick's API key = 759292a3
+//* Darrin's OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=c82fba9d 
+
+//*TMDB
+//* Darrin's key 92a965805ccb832e42831c5c79bc1c67
+//* example endpoint https://api.themoviedb.org/3/movie/550?api_key=92a965805ccb832e42831c5c79bc1c67
+
 
 //* Create API endpoint URL
-var urlMovies = 'http://www.omdbapi.com/?apikey=759292a3&';
+//var urlMovies = 'http://www.omdbapi.com/?apikey=759292a3r=json&type=movie&genre=action&';
+//OMDB
+//var urlMovies = 'http://www.omdbapi.com/?apikey=c82fba9d&s=type&type=movie&genre=action&y=1981&page=100';
 
+//* Going forward with TMDB 
+//TMDB
+//var urlMovies = 'https://api.themoviedb.org/3/movie/550?api_key=92a965805ccb832e42831c5c79bc1c67'
+//* This will give us 10000 results for genre = drama
+var urlMoviesDrama = 'https://api.themoviedb.org/3/discover/movie?with_genres=18&api_key=92a965805ccb832e42831c5c79bc1c67&language=en-US'
 var responseText = document.getElementById('response-text');
 
-//var responseText = document.getElementById('response-text');
+//*create a list to put our data in 
+var mediaList = document.querySelector('ul');
 
-function getApi(urlMovies) {
-  fetch(urlMovies)
+
+function getApi(urlMoviesDrama) {
+  fetch(urlMoviesDrama)
     .then(function (response) {
       console.log(response);
       // We check whether the response.status equals 200, as follows:
@@ -20,10 +36,20 @@ function getApi(urlMovies) {
       }
       // we return response.json()
       return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var listItem = document.createElement('li');
+        listItem.textContent = data[i].html_url;
+        mediaList.appendChild(listItem);
+      }
+
+
   });
 }
 
-getApi(urlMovies);
+getApi(urlMoviesDrama);
 
 
     //* create var for API key
